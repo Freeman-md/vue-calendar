6a4067800ae1ae0458a64c46c779d8ee255a089d
Freeman-md <freemansparda@gmail.com>
2021-06-22 14:37:08 +0100

    Add Event

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -5,13 +5,14 @@
         <v-toolbar
           flat
         >
-          <!-- <v-btn
+          <v-btn
               color="primary"
               class="mr-4"
-              @click="setToday"
+              @click="dialog = true"
+              dark
             >
             New Event
-          </v-btn> -->
+          </v-btn>
           <v-btn
             outlined
             class="mr-4"
@@ -81,6 +82,30 @@
           </v-menu>
         </v-toolbar>
       </v-sheet>
+
+      <!-- Add Event Dialog -->
+      <v-dialog v-model="dialog" max-width="500">
+        <v-card>
+          <v-container>
+            <v-form @submit.prevent="addEvent">
+              <v-text-field v-model="name" type="text" label="Event Name (required)"></v-text-field>
+              <v-text-field v-model="details" label="Detail"></v-text-field>
+              <v-text-field v-model="start" type="date" label="Start (required)"></v-text-field>
+              <v-text-field v-model="end" type="date" label="End (required)"></v-text-field>
+              <v-text-field v-model="color" type="color" label="Color (click to open color menu)"></v-text-field>
+              <v-btn 
+              type="submit" 
+              color="primary" 
+              class="mr-4" 
+              @click.stop="dialog = false"
+              >
+                Create Event
+              </v-btn>
+            </v-form>
+          </v-container>
+        </v-card>
+      </v-dialog>
+
       <v-sheet height="600">
         <v-calendar
           ref="calendar"
@@ -198,6 +223,25 @@ import { db } from '@/main'
           events.push(appData)
         }),
         this.events = events
+      },
+      async addEvent() {
+        if (this.name && this.start && this.end)  {
+          await db.collection('calEvent').add({
+            name: this.name,
+            details: this.details,
+            start: this.start,
+            end: this.end,
+            color: this.color
+          })
+          this.getEvents()
+          this.name = null
+          this.details = null
+          this.start = null
+          this.end = null
+          this.color = '#1976D2'
+        } else {
+          alert('Name, start and end date are required')
+        }
       },
       async updateEvent(ev) {
         await db
